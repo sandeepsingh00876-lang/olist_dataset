5320d723b2a0ca5661a22975b5c65bf254a307e3
sandeepsingh00876-lang <sandeepsingh00876@gmail.com>
2026-09-02 17:25:04 +0530

    updated java script

--- javascript.js
+++ javascript.js
@@ -1,7 +1,23 @@
 fetch("seller vs total_revenue.csv")
-    .then(Response => Response.text())
+    .then(response => response.text())
     .then(data => {
-        const rows = data.split("\n");
-        console.log(rows);
 
+        const rows = data.split("\n");
+
+        const headers = rows[0].split(",");
+
+        console.log(headers);
+
+        for (let i = 1; i < rows.length; i++) {
+
+            const row = rows[i].split(",");
+            if (rows[i].trim() === "") {
+        continue;
+    }
+
+            const seller = row[0];
+            const revenue = Number(row[1]);
+
+            console.log(seller, revenue);
+        }
     });
