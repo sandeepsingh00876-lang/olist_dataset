624b3199fd2187abd650f8b8af90e30fa0ce083c
sandeepsingh00876-lang <sandeepsingh00876@gmail.com>
2026-09-02 22:07:05 +0530

    second changes

--- javascript.js
+++ javascript.js
@@ -1,31 +1,31 @@
-f\
-
-
-
-
-
-
-
-
-
-
-\\etch("seller vs total_revenue.csv")
+fetch("seller vs total_revenue.csv")
     .then(response => response.text())
     .then(data => {
 
         const rows = data.trim().split("\n");
 
-        let sellers = [];
-        let revenues = [];
-
+        
+        let sellerdata = []
         for (let i = 1; i < rows.length; i++) {
 
             const row = rows[i].split(",");
 
-            sellers.push(row[0]);
-            revenues.push(Number(row[1]));
+            const seller = row[0];
+            const revenue = Number(row[1]);
+            sellerdata.push({
+                seller: seller,
+                revenue: revenue
+            });
 
         }
+
+        //sort the sellerdata array in descending order based on revenue
+        sellerdata.sort((a, b) => b.revenue - a.revenue);
+        // get the top 10 sellers
+        sellerdata = sellerdata.slice(0, 10);
+
+        const sellers = sellerdata.map(item => item.seller);
+        const revenues = sellerdata.map(item => item.revenue);
 
         new Chart(document.getElementById("sellerChart"), {
 
@@ -45,5 +45,27 @@ f\
             }
 
         });
+
+    });
+
+    fetch("seller vs total_revenue.csv")
+    .then(response => response.text())
+    .then(data =>{
+        const rows = data.trim().split("\n");
+
+        let orderdata = []
+        for (let i = 1; i < rows.length; i++) {
+
+            const row = rows[i].split(",");
+
+            const seller = row[0];
+            const total_order = Number(row[1]);
+            orderdata.push({
+                seller: seller,
+                total_order: total_order
+            });
+
+        }
+    console.log(orderdata)
 
     });
